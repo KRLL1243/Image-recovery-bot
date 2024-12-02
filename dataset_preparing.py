40f8826ae639a4909d4cf327733a0cd45ced6a36
KRLL1243 <KRLL132445@gmail.com>
2024-12-02 22:25:36 +0300

    commit

--- dataset_preparing.py
+++ dataset_preparing.py
@@ -3,52 +3,43 @@ from PIL import Image, ImageFilter
 import random
 from tqdm import tqdm
 
-# Путь к оригинальным изображениям
+# Path to folder with original images
 DATASET_PATH = "img_align_celeba"
+
+# Path to the folder for processed images
 OUTPUT_PATH = "prepared_dataset"
 os.makedirs(OUTPUT_PATH, exist_ok=True)
 
 
-# Функция для добавления повреждений
-def add_defects(image: Image.Image):
-    # Преобразование в черно-белое
-    # if random.random() > 0.5:
-    #     image = image.convert("L").convert("RGB")
-
-    # Добавление размытия
-    # if random.random() > 0.5:
+# Function for adding blur
+def add_blur(image: Image.Image):
     image = image.filter(ImageFilter.GaussianBlur(radius=random.uniform(2, 5)))
-
-    # Добавление шума
-    # if random.random() > 0.4:
-    #     noise = Image.effect_noise(image.size, random.randint(20, 50))
-    #     image = Image.blend(image, noise.convert("RGB"), alpha=0.5)
 
     return image
 
 
-# Подготовка датасета
+# Dataset preparation
 def prepare_dataset(dataset_path, output_path, image_size=256):
     image_files = os.listdir(dataset_path)
     for image_file in tqdm(image_files, desc="Preparing dataset"):
         try:
-            # Загрузка изображения
+            # Uploading an image
             image_path = os.path.join(dataset_path, image_file)
             original = Image.open(image_path).convert("RGB").resize((image_size, image_size))
 
-            # Создание поврежденного изображения
-            damaged = add_defects(original)  # Правая половина
+            # Creating a blurred image
+            blurred = add_blur(original)
 
-            # Создание парного изображения
+            # Creating a paired image
             combined = Image.new("RGB", (image_size * 2, image_size))
-            combined.paste(damaged, (0, 0))  # Поврежденное слева
-            combined.paste(original, (image_size, 0))  # Оригинал справа
+            combined.paste(blurred, (0, 0))  # Blurred left
+            combined.paste(original, (image_size, 0))  # Original right
 
-            # Сохранение
+            # Save
             combined.save(os.path.join(output_path, image_file))
         except Exception as e:
-            print(f"Ошибка с файлом {image_file}: {e}")
+            print(f"Error with file {image_file}: {e}")
 
 
-# Запуск подготовки
+# Start preparation
 prepare_dataset(DATASET_PATH, OUTPUT_PATH)
